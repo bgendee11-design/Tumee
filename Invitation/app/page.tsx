2f5a0704bed826fe9f515e0f217ea1583bbb0f86
bgendee11-design <bgendee11@gmail.com>
2026-08-17 20:50:18 +0800

    шшшш

--- Invitation/app/page.tsx
+++ Invitation/app/page.tsx
@@ -5,6 +5,45 @@ import GallerySection from "./components/GallerySection";
 import HeroSection from "./components/HeroSection";
 import RSVPSection from "./components/RSVPSection";
 
+export async function generateMetadata() {
+  const title = `Сэвлэг үргээх ёслол`;
+  const description = "Сэвлэг үргээх ёслолд хүрэлцэн ирэхийг урьж байна.";
+
+  const url = `https://tumee-invitation.vercel.app`;
+
+  return {
+    title,
+    description,
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      type: "website",
+      locale: "mn_MN",
+      url,
+      siteName: "Сэвлэг үргээх ёслолын урилга",
+      title,
+      description,
+      images: [
+        {
+          url: "https://tumee-invitation.vercel.app/inv2.jfif",
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [
+        "https://tumee-invitation.vercel.app/inv2.jfif",
+      ],
+    },
+  };
+}
+
 
 export default function Home() {
   return (
